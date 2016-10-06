11c06635508e585381a21e2b6da85595ff77f901
nosami <jasonimison@gmail.com>
2016-10-06 13:16:11 +0100

    first commit

---
+++ iOS/AppDelegate.fs
@@ -0,0 +1,23 @@
+﻿namespace bug42986.iOS
+
+open System
+open UIKit
+open Foundation
+open Xamarin.Forms
+open Xamarin.Forms.Platform.iOS
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit FormsApplicationDelegate ()
+
+    override this.FinishedLaunching (app, options) =
+        Forms.Init()
+        this.LoadApplication (new bug42986.App())
+        base.FinishedLaunching(app, options)
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main(args, null, "AppDelegate")
+        0
+
